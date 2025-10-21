5534c9cfd696a187362a6ee35b0b0e11c0bac8dd
Juloup78 <81333432+Juloup78@users.noreply.github.com>
2025-10-21 14:30:15 +0200

    types
    struct

--- utils.h
+++ utils.h
@@ -1,5 +1,18 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
+#ifndef UTILS_H
+#define UTILS_H
 
+typedef struct cell {
+    int arrival_vertex;
+    float probability;
+    struct cell* next_edge;
+} cell;
 
+typedef struct list {
+    struct cell* head;
+} plist;
+
+typedef struct adjacency_list {
+    struct cell* edges[100];
+    int size;
+} adjacency_list;
 #endif
