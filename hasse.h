65da671a2def04253a90c06297889d605e9d82bc
Juloup78 <81333432+Juloup78@users.noreply.github.com>
2025-10-21 15:44:30 +0200

    Add Markov graph data structures and function declarations to hasse.h

--- hasse.h
+++ hasse.h
@@ -1,14 +1,33 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
-void removeTransitiveLinks(t_link_array *p_link_array);
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * @brief Creates a link array from the given partition and graph.
- *
- * @param part The partition of the graph.
- * @param graph The adjacency list representation of the graph.
- * @return The created link array.
- */
+typedef struct cell {
+    int arrival_vertex;
+    float probability;
+    struct cell *next;
+} cell;
+
+typedef struct list {
+    cell *head;
+} list;
+
+typedef struct adjacency_list {
+    int nb_vertices;
+    list *lists;
+} adjacency_list;
+
+cell *create_cell(int arrival_vertex, float probability);
+list *create_empty_list();
+void add_cell_to_list(list *l, int arrival_vertex, float probability);
+void display_list(list *l);
+adjacency_list *create_empty_adjacency_list(int nb_vertices);
+void display_adjacency_list(adjacency_list *adj_list);
+adjacency_list *readGraph(const char *filename);
+void is_markov_graph(adjacency_list *adj_list);
+char *getId(int num);
+void generate_mermaid_file(adjacency_list *adj_list, const char *filename);
 
 #endif
